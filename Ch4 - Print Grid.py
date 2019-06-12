5688be02db1662f184f5cef5d0e292b926d3bac7
Ivan Veselov <veselov@gmail.com>
2019-06-12 21:20:21 +0100

    Another set of changes

--- Ch4 - Print Grid.py
+++ Ch4 - Print Grid.py
@@ -8,7 +8,7 @@
 #        ['O', 'O', 'O', 'O', '.', '.'],
 #        ['.', 'O', 'O', '.', '.', '.'],
 #        ['.', '.', '.', '.', '.', '.']]
-#You can think of grid[x][y] as being the character at the x- and y-coordinates of a “picture” drawn with text characters. 
+#You can think of grid[x][y] as being the character at the x- and y-coordinates of a “picture” drawn with text characters.
 #The (0, 0) origin will be in the upper-left corner, the x-coordinates increase going right, and the y-coordinates increase going down.
 #Copy the previous grid value, and write code that uses it to print the image.
 #..OO.OO..
@@ -28,10 +28,22 @@ grid = [['.', '.', '.', '.', '.', '.'],
         ['.', 'O', 'O', '.', '.', '.'],
         ['.', '.', '.', '.', '.', '.']]
 
-for j in range (len(grid[0])):
-    for i in range (len(grid)):
-        if i != len(grid) - 1:
-             print (grid[i][j],end='')
-        else:
-            print (grid[i][j])
-            
+# for j in range(len(grid[0])):
+#     for i in range(len(grid)):
+#         if i != len(grid) - 1:
+#              print (grid[i][j], end='')
+#         else:
+#             print (grid[i][j])
+
+
+# variant #2
+def test():
+    for row in grid:
+        cnt = 0
+        for cell in row:
+            cnt += 1
+            if cnt == len(row):
+                print(cell)
+            else:
+                print(cell, end='')
+
--- rook_moves.py
+++ rook_moves.py
@@ -1,14 +1,31 @@
-def rookMoves (position):
-    boardHoriz = ["a","b","c","d","e","f","g","h"]
+def cell_to_coord(_cell):
+    # example: takes "a1", returns (0, 0)
+    return (1, 1)
+
+def coord_to_cell(i, j):
+    # takes (0, 0), returns "a1"
+    return
+
+
+def rookMoves(position):
+    boardHoriz = ["a", "b", "c", "d", "e", "f", "g", "h"]
+    # boardHoriz = "a b c d e f g h".split()
+
     acceptableMoves = []
+
     for i in range(8):
-        acceptableMoves.append(position[0] + str(i+1))
+        acceptableMoves.append(position[0] + str(i + 1))
         acceptableMoves.append(boardHoriz[i] + position[1])
+
     acceptableMoves.remove(position)
     acceptableMoves.remove(position)
     acceptableMoves.sort()
     return acceptableMoves
 
-print ("Input Rook position on a chess board")
-position = input()
-print (rookMoves (position))
+def main():
+    print ("Input Rook position on a chess board")
+    position = input()
+    print (rookMoves (position))
+
+if __name__ == "__main__":
+    main()
