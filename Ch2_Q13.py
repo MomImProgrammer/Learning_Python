9897fd215ef1e3222ddf02e0023648c345b86c71
Ivan Veselov <veselov@gmail.com>
2019-06-12 20:52:32 +0100

    Many changes following code review

--- Ch2_Q13.py
+++ Ch2_Q13.py
@@ -1,9 +1,9 @@
-#Q:13. Write a short program that prints the numbers 1 to 10 using a for loop. 
+#Q:13. Write a short program that prints the numbers 1 to 10 using a for loop.
 #Then write an equivalent program that prints the numbers 1 to 10 using a while loop.
 
 for i in range(1, 11):
     print(i)
 i = 1
-while i <=10:
+while i <= 10:
     print(i)
-    i = i+1
+    i = i + 1
--- Ch4 - Comma Code.py
+++ Ch4 - Comma Code.py
@@ -1,5 +1,7 @@
-#Write a function that takes a list value as an argument and returns a string with all the items separated by a comma and a space, 
-#with and inserted before the last item. 
+'''
+Write a function that takes a list value as an argument and returns a string with all the items separated by a comma and a space,
+with and inserted before the last item.
+'''
 
 def ListToCommaString(list):
     numOfElements = len(list)
@@ -13,6 +15,44 @@ def ListToCommaString(list):
             string = string + list[i]
     return string
 
-list = ['apples', 'bananas', 'tofu', 'cats']
-string = ListToCommaString(list)
-print (string)
+
+def ListToCommaString2(list):
+    numOfElements = len(list)
+    strings = []
+    for i in range(numOfElements):
+        limit = numOfElements - 2
+        strings.append(list[i])
+        if i < limit:
+            strings.append(', ')
+        elif i == limit:
+            strings.append(' and ')
+    return ''.join(strings)
+
+def sample(num_of_strings, str_len):
+    res = []
+    for _ in range(num_of_strings):
+        res.append(''.join(random.choices("abcd", k=str_len)))
+
+    return res
+
+def test(num_of_tests):
+    for _ in range(num_of_tests):
+        s = sample(5, 3)
+        if ListToCommaString(s) == ListToCommaString2(s):
+            print("[OK]:", s)
+        else:
+            print("[FAIL]:", s)
+
+###############################################################
+## Runs only when called as `python 'Ch4 - Comma Code.py'`
+##
+
+def main():
+    print('hello')
+    # run tests
+    list = ['apples', 'bananas', 'tofu', 'cats']
+    string = ListToCommaString(list)
+    print (string)
+
+if __name__ == "__main__":
+    main()
