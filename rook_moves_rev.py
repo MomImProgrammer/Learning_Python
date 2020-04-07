bfaf61bf18a5a10ac186da62d74e69e877d90411
Lena <51487834+MomImProgrammer@users.noreply.github.com>
2020-04-07 18:34:04 +0100

    Add files via upload
    Changed after receiving comments and added display function

--- rook_moves_rev.py
+++ rook_moves_rev.py
@@ -2,7 +2,7 @@ from string import ascii_lowercase
 
 def validate(rook_position):
     if  rook_position[0] in (ascii_lowercase[0:8]) and int(rook_position[1]) in range(1,9):
-        return True
+        return ''
     else:
         return 'Your rook is not on the board. Try again.'
 
@@ -16,18 +16,29 @@ def rook_moves(rook_position):
             moves.append(j+rook_position[1])
     return moves
 
-def display_moves(rook_moves_list):
-    # to do
+def display_moves(position, moves_list):
+    for i in range(8,0,-1):
+        for j in (ascii_lowercase[0:8]):
+            cell = j+str(i)
+            if cell == position:
+                print('o', end='')
+            elif cell in moves_list:
+                print('x', end='')
+            else:
+                print('.', end='')
+            if j == ascii_lowercase[7]:
+                print('\n', end='')
     return
 
 def main():
-    print ('Input rook position:')
+    print('Input rook position:')
     rook_position = input()
-    if validate(rook_position) == True:
+    validation_error = validate(rook_position)
+    if  validation_error == '':
         rook_moves_list = rook_moves(rook_position)
         print (rook_moves_list)
-    else: print(validate(rook_position))
-    # display_moves(rook_moves_list)
+    else: print(validation_error)
+    display_moves(rook_position, rook_moves_list)
 
 if __name__ == "__main__":
     main()
