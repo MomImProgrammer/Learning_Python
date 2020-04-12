90cff2070d8937858cf2fbc4b28dc657030da7f1
lena <heleanor@gmail.com>
2020-04-12 09:23:11 +0100

    added a comment

--- ch2_q9.py
+++ ch2_q9.py
@@ -1,6 +1,6 @@
 # Q9. Write code that prints Hello if 1 is stored in spam, prints Howdy if 2 is stored in spam, and prints Greetings! 
 # if anything else is stored in spam.
-# another comment
+# another comment test
 spam = input()
 if spam == '1':
     print('Hello')
