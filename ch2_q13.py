77a29c16e6cfb3a677029e95567d2a483905b676
lena <heleanor@gmail.com>
2020-04-12 09:01:13 +0100

    added a comment

--- ch2_q13.py
+++ ch2_q13.py
@@ -1,6 +1,5 @@
 #Q:13. Write a short program that prints the numbers 1 to 10 using a for loop.
-#Then write an equivalent program that prints the numbers 1 to 10 using a while loop.
-
+#Then write an equivalent program that prints the numbers 1 to 10 using a while loop. test
 for i in range(1, 11):
     print(i)
 i = 1
